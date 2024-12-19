3886e0d33d6db7ebd061297a6da90336cb3c4024
ygorsimoura <ygorsimoura01@gmail.com>
2024-12-18 22:35:00 -0300

    Preenchendo campos obrigatórios

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -7,8 +7,18 @@
 
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verificar o título da aplicacão', function() {
+    beforeEach(function() {
         cy.visit('./src/index.html')
+    })
+    it('verificar o título da aplicacão', function() {
         cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
+    })
+    it('preencher os campos obrigatórios e envia o formulário', function() {
+        cy.get('input#firstName').type('Ygor')
+          .get('input#lastName').type('Simoura')
+          .get('input#email').type('ygorsimoura@gmail.com')
+          .get('textarea#open-text-area').type('Escrevendo apenas um teste')
+          .get('button.button').click()
+          .get('span.success').should('be.visible')
     })
 })
