61d0dee71c611f548a94d05272c78abb0d3787c5
ygorsimoura <ygorsimoura01@gmail.com>
2024-12-22 13:11:42 -0300

    Caso de teste que verifica que a política de privacidade abre em outra aba sem a necessidade de um clique

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -117,11 +117,14 @@ describe('Central de Atendimento ao Cliente TAT', function() {
           expect($input[0].files[0].name).to.equal('example.json')
         })    
     })
-    it.only('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', function(){
+    it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', function(){
       cy.fixture('example.json').as('sampleFile')
       cy.get('#file-upload').selectFile('@sampleFile')
       .should(function($input){
         expect($input[0].files[0].name).to.equal('example.json')
       })
+    })
+    it.only('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function(){
+      cy.get('a').should('have.attr','target','_blank')
     })
 })
