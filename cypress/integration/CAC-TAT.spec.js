e60bea8e06fa116bf18b7d978c21d3ec28ac4b3e
ygorsimoura <ygorsimoura01@gmail.com>
2024-12-23 12:20:28 -0300

    update: Adicionando comandos de tempo

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -7,7 +7,8 @@
 
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-    beforeEach(function() {
+  const THREE_SECONDS_IN_MS = 3000  
+  beforeEach(function() {
         cy.visit('./src/index.html')
     })
     it('verificar o título da aplicacão', function() {
@@ -15,20 +16,26 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
     it('preencher os campos obrigatórios e envia o formulário', function() {
        const longText = 'Lorem ipsum dolor sit amet. Id nostrum placeat in autem iusto et adipisci itaque et assumenda animi in nihil esse est voluptatem nulla? Ab quibusdam perferendis sed aspernatur repudiandae aut eligendi enim sit saepe autem.'
-        cy.get('input#firstName').type('Ygor')
+       cy.clock() 
+       cy.get('input#firstName').type('Ygor')
           .get('input#lastName').type('Simoura')
           .get('input#email').type('ygorsimoura@gmail.com')
           .get('textarea#open-text-area').type(longText, { delay: 0})
           .get('button.button').click()
           .get('span.success').should('be.visible')
+        cy.tick(THREE_SECONDS_IN_MS)
+        cy.get('span.success').should('not.be.visible')
     })
     it('exibe menssagem de erro ao submeter o formulário com um e-mail com formatacão inválida', function(){
-        cy.get('input#firstName').type('Ygor')
+      cy.clock()  
+      cy.get('input#firstName').type('Ygor')
           .get('input#lastName').type('Simoura')
           .get('input#email').type('ygorsimouragmail.com')
           .get('textarea#open-text-area').type('teste', { delay: 0})
         cy.contains('button', 'Enviar').click()
           .get('span.error').should('be.visible')
+        cy.tick(THREE_SECONDS_IN_MS)
+        cy.get('span.error').should('not.be.visible')
     })
     it('campo de telefone continua vazio quando preechido com valor não numérico', function() {
         cy.get('#phone')
@@ -36,13 +43,17 @@ describe('Central de Atendimento ao Cliente TAT', function() {
           .should('have.value', '')
     })
     it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function() {
-        cy.get('input#firstName').type('Ygor')
+      cy.clock()  
+      cy.get('input#firstName').type('Ygor')
           .get('input#lastName').type('Simoura')
           .get('input#email').type('ygorsimoura@gmail.com')
           .get('#phone-checkbox').check()
           .get('textarea#open-text-area').type('teste', { delay: 0})
         cy.contains('button', 'Enviar').click()
           .get('span.error').should('be.visible')
+        cy.tick(THREE_SECONDS_IN_MS)
+        cy.get('span.error').should('not.be.visible')
+
 
     })
     it('preenche e limpa os campos nome, sobrenome, email e telefone', function(){
@@ -65,13 +76,20 @@ describe('Central de Atendimento ao Cliente TAT', function() {
    
     })
     it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function(){
-        cy.contains('button', 'Enviar')
+      cy.clock()  
+      cy.contains('button', 'Enviar')
           .click()
           .get('span.error').should('be.visible')
+      cy.tick(THREE_SECONDS_IN_MS)
+      .get('span.error').should('not.be.visible')
+
     })
     it('envia formulário com sucesso usando um comando costomizado', function(){
-        cy.fillMandatoryFieldsAndSubmit()
+      cy.clock()  
+      cy.fillMandatoryFieldsAndSubmit()
         cy.get('.success').should('be.visible')
+      cy.tick(THREE_SECONDS_IN_MS)
+      cy.get('.success').should('not.be.visible')
     })
     it('seleciona um produto (Youtube) por seu texto', function(){
       cy.get('select').select('YouTube')
@@ -131,4 +149,5 @@ describe('Central de Atendimento ao Cliente TAT', function() {
       cy.get('a').invoke('removeAttr','target').click()
       cy.url().should('include', '/privacy')
     })
+    
 })
